0d98e50447ae6de0d6fcd96476e053b3d6e037ff
Umakant Dwivedi <78226651+ukdgit@users.noreply.github.com>
2023-07-07 16:08:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,5 @@
-function decimalToBinary(num){
-    let binaryStr = ""
-    while(num>0){
-        let rem = num%2;
-        binaryStr = binaryStr + rem.toString()
- 
-        num = Math.floor(num/2)
-    }
-  
-    return binaryStr.split("").reverse().join("")
+function decimalToBinary(num) {
+  // Write your code here
 }
- 
-module.exports = decimalToBinary;
+
+window.decimalToBinary = decimalToBinary;
